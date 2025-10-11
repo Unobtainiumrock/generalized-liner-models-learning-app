e17e4f53296190be4f49769b58d74c0a18e0474b
Nicholas Fleischhauer <unobtainiumrock@gmail.com>
2025-10-10 23:37:29 -0700

    Fix missing link functions in glm.ts (identity, logit, probit) and remove Slider test

--- src/lib/glm.ts
+++ src/lib/glm.ts
@@ -10,8 +10,26 @@ import {
 } from '@/types';
 
 const inverseLinkFunctions = {
+  identity: (x: number) => x,
   log: (x: number) => Math.exp(x),
+  logit: (x: number) => 1 / (1 + Math.exp(-x)),
   inverse: (x: number) => 1 / x,
+  probit: (x: number) => {
+    const a1 =  0.254829592;
+    const a2 = -0.284496736;
+    const a3 =  1.421413741;
+    const a4 = -1.453152027;
+    const a5 =  1.061405429;
+    const p  =  0.3275911;
+    
+    const sign = x >= 0 ? 1 : -1;
+    x = Math.abs(x) / Math.sqrt(2.0);
+    
+    const t = 1.0 / (1.0 + p * x);
+    const y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * Math.exp(-x * x);
+    
+    return 0.5 * (1.0 + sign * y);
+  },
   cloglog: (x: number) => 1 - Math.exp(-Math.exp(x)),
 };
 
